2898022de8e694b2bb7e0ea53514aedc39c41f67
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2022-03-22 23:54:54 +0200

    Add files via upload

---
+++ Day6/squareRoot.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+double SqrRoot(int*);
+
+int main()
+{   
+    int n = 145;
+    printf ("Square root of %d is : %lf\n", n, SqrRoot(&n));
+
+    return 0;
+
+}
+
+double SqrRoot(int* n){
+double a = *n;
+double b = 0.1;
+double e = 0.000001;
+
+    while ((a-b)>e)
+    {
+        a = (a+b)/2;
+        b = *n/a;
+    }
+    
+    return a;
+}
