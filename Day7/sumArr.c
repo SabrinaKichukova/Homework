8e1868c7f8fbb09e3cfba3eff150dc26de15422c
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2022-03-26 19:00:27 +0200

    Add files via upload

---
+++ Day7/sumArr.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+unsigned sumArrayDigits(const int* arr, size_t n);
+
+int main()
+{   
+    const int arr[] = {12, 34, 5, 70 };
+
+    printf("%d\n",sumArrayDigits(arr, 4));
+    
+    return 0;
+}
+
+unsigned sumArrayDigits(const int* arr, size_t n){
+
+    int sum = 0;
+    int rem = 0;
+
+    for(int i = 0; i < n; i++){
+
+        int num = *(arr + i);
+
+        while(num){
+
+        rem = num % 10;
+        sum += rem;
+        num = num /10;
+    
+        }
+    
+    }
+    return sum;
+}
