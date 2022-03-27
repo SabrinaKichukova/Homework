8d743a5c2601021cc68b8086c58c995ed0a1b9ff
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2022-03-27 10:38:31 +0300

    Add files via upload

---
+++ Day6/sinFunc.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define TERMS 7
+
+float sine(int);
+int fact(int);
+float power(float, int);
+
+int main()
+{   int deg;
+    scanf ("%d", &deg);
+    printf("%f",sine(deg));
+    return 0;
+}
+    
+float sine(int deg) {
+    double pi = 3.14159;
+    deg %= 360;
+    float rad = deg * pi / 180;
+    float sin = 0;
+
+    int i;
+    for(i = 0; i < TERMS; i++) { 
+        sin += power(-1, i) * power(rad, 2 * i + 1) / fact(2 * i + 1);
+    }
+    return sin;
+}
+
+    float power(float base, int exp) {
+    if(exp < 0) {
+        if(base == 0)
+            fprintf(stderr,"Error! Base = 0!")   
+            return -0; 
+        return 1 / (base * power(base, (-exp) - 1));
+    }
+    if(exp == 0)
+        return 1;
+    if(exp == 1)
+        return base;
+    return base * power(base, exp - 1);
+}
+
+int fact(int n) {
+    return n <= 0 ? 1 : n * fact(n-1);
+}
