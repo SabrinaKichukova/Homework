253de3f25fc2e75bbaad5c54efc652af0b3a6688
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2022-03-27 18:54:27 +0300

    Add files via upload

---
+++ Day10/realtobit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <float.h>
+
+union realtobit
+{
+    int32_t a;
+    float b;
+
+} real1;
+
+int main(){
+
+    real1.a = 0;
+   
+    printf("Please enter real number:\n");
+    scanf("%f",&real1.b);
+
+    for (int i = 31; i >= 0; i--)
+  {
+    int k = real1.a >> i;
+
+    if (k & 1)
+      printf("1");
+    else
+      printf("0");
+  }
+  printf("\n");
+
+    return 0;
+}
