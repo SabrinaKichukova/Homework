619cb0bb1a0d2100869aa08b1b2fad200654404c
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2022-03-29 22:45:45 +0300

    Add files via upload

---
+++ Day11/preproc.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#define CLEARBIT(MASK,BIT) ((MASK)  &= (~(1 << (BIT))))
+#define CHANGEBIT(MASK,BIT) ((MASK) ^= (1 << (BIT)))
+#define SETBIT(MASK,BIT) ((MASK) |= (1 << (BIT)))
+#define CHECKBIT(MASK,BIT) (!!((MASK) & (1 << (BIT)))) 
+#define MAX(A,B) ((A)>(B)?(A):(B))
+#define MAX3(A,B,C) (MAX(MAX((A),(B)),(C))
+#define MIN(A,B) ((A)>(B)?(A):(B))
+#define MIN3(A,B,C) (MIN(MIN((A),(B)),(C))
+#define SWAPVARS(A,B,TYPE) (TYPE TEMP = (A); (A)=(B);(B)=TEMP;)
+
+
+int main()
+{
+    int mask = 0x00;
+    int bit = 1;
+  
+
+    SETBIT(mask,bit);
+    printf("%x \n",mask);
+
+    CLEARBIT(mask,bit);
+    printf("%x \n",mask);
+
+     if (CHECKBIT(mask,bit) == 0){
+        printf("0\n");
+    } else {
+        printf("1\n");
+    }
+
+    CHANGEBIT(mask,bit);
+    printf("%x \n",mask);
+
+    if (CHECKBIT(mask,bit)== 0){
+        printf("0");
+    } else {
+        printf("1");
+    }
+   
+return 0;
+}
