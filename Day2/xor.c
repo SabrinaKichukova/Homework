d016af9fe3552c57e0b2113fb04884c05c34454b
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-25 15:37:51 +0200

    Better solution added

--- Day2/xor.c
+++ Day2/xor.c
@@ -1,4 +1,4 @@
-/* 
+/*
 Author: Sabrina Kichukova
 Purpose: This program calculate XOR in 2 modes for 2 and 3 digit.
 */
@@ -8,99 +8,52 @@ Purpose: This program calculate XOR in 2 modes for 2 and 3 digit.
 
 int main()
 {
-    
     int a,b,c;
     int mode;
+    int result;
 
-    printf("\nFor 2 digits OR enter 2, for 3 digit OR enter 3.\n");
-
+    printf("\nFor 2 digits XOR mode enter 2, for 3 XOR mode enter 3.\n");
     scanf("%d",&mode);
 
-    if ((mode != 2) && (mode != 3)){
-    
-    fprintf(stderr,"Invalid number!"); // Error!
-    return EXIT_FAILURE;
+    if ((mode != 2) & (mode != 3))
+    {
+        fprintf(stderr,"Invalid mode!");
+        return EXIT_FAILURE;
     }
 
-    if(mode == 2){
+    if(mode == 2)
+    {
+        printf("\nEnter two numbers: 0 or 1\n");
+        scanf("%d %d",&a,&b);
 
-    printf("\nEnter two numbers: 0 or 1\n");
-    scanf("%d %d",&a,&b);
-
-    if ((a!=0) && (b!=0) && (a!=1) && (b!=1)){
-
-    fprintf(stderr,"Invalid numbers!\n"); // Error!
-    return EXIT_FAILURE;
-
+        if ((a!=0) && (b!=0) && (a!=1) && (b!=1))
+        {
+            fprintf(stderr,"Invalid numbers!\n");
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            result = ((a & ~b) || (~a & b));
+            printf ("%d\n", result);
+        }
     }
-    
-    else if (a==1 && b == 1){
 
-    printf("0\n");
+    if (mode ==3)
+    {
+        printf("Enter three numbers: 0 or 1\n");
+        scanf("%d %d %d",&a,&b,&c);
 
-    } else if (a==1 && b == 0){
-
-    printf("1\n");
-
-    } else if (a== 0 && b == 1){
-    
-    printf("1\n");
-
-    } else if (a==0 && b == 0){
-    
-    printf("0\n");
-
+        if (((a!=0) && (b!=0) && (c!=0)) && ((a!=1) && (b!=1) && (c!=1)))
+        {
+            fprintf(stderr, "Invalid numbers!\n"); // Error!
+            return EXIT_FAILURE;
+        }
+        else
+        {
+            result = ((a & ~b & ~c) | (~a & b & ~c) | (~a & ~b & c) | (a & b & c));
+            printf ("%d\n", result);
+        }
     }
-}
 
-    if (mode ==3){
-
-    printf("Enter three numbers: 0 or 1\n");
-
-    scanf("%d %d %d",&a,&b,&c);
-
-    if (((a!=0) && (b!=0) && (c!=0)) && ((a!=1) && (b!=1) && (c!=1))){
-
-    fprintf(stderr, "Invalid numbers!\n"); // Error!
-    return EXIT_FAILURE;
-
-    } 
-    else {
-
-    if ((a==1) && (b == 1) && (c==1)){
-
-    printf("0\n");
-
-    } else if ((a==1) && (b == 0) && (c==0)){
-
-    printf("1\n");
-
-    } else if ((a== 0) && (b == 1) && (c==0)){
-
-    printf("1\n");
-
-    } else if ((a==0) && (b == 0) && (c==0)){
-
-    printf("0\n");
-
-    } else if ((a==0) && (b == 1) && (c==1)){
-
-    printf("0\n");
-
-    } else if ((a==1) && (b == 0) && (c==1)){
-
-    printf("0\n");
-
-    } else if ((a==0) && (b == 1) && (c==1)){
-
-    printf("0\n");
-
-    } else if ((a==0) && (b == 0) && (c==1)){
-
-    printf("1\n");
-    }
-}
-}
-    
     return EXIT_SUCCESS;
 }
