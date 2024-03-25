559c77ee6bfcee9a6b5094fbfb213abf99cf73df
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-25 15:41:20 +0200

    Better solution added

--- Day2/xor.c
+++ Day2/xor.c
@@ -24,9 +24,9 @@ int main()
     if(mode == 2)
     {
         printf("\nEnter two numbers: 0 or 1\n");
-        scanf("%d %d",&a,&b);
+        scanf("%d %d", &a, &b);
 
-        if ((a!=0) && (b!=0) && (a!=1) && (b!=1))
+        if ((a != 0) && (b != 0) && (a != 1) && (b != 1))
         {
             fprintf(stderr,"Invalid numbers!\n");
             return EXIT_FAILURE;
@@ -41,11 +41,11 @@ int main()
     if (mode ==3)
     {
         printf("Enter three numbers: 0 or 1\n");
-        scanf("%d %d %d",&a,&b,&c);
+        scanf("%d %d %d", &a, &b, &c);
 
-        if (((a!=0) && (b!=0) && (c!=0)) && ((a!=1) && (b!=1) && (c!=1)))
+        if (((a != 0) && (b != 0) && (c != 0)) && ((a != 1) && (b != 1) && (c != 1)))
         {
-            fprintf(stderr, "Invalid numbers!\n"); // Error!
+            fprintf(stderr, "Invalid numbers!\n");
             return EXIT_FAILURE;
         }
         else
