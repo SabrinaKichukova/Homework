8612055ad316f0017dd64d201232ecfc5fdc0f1e
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-26 14:27:53 +0200

    Update rectangle.c

--- Day5/rectangle.c
+++ Day5/rectangle.c
@@ -14,27 +14,27 @@ int main()
     
     printf("\nPlease, enter a, b:\n");
 
-    while (scanf("%lf %lf", &a,&b) != EOF){
-        if((a||b)<=0){
+    while (scanf("%lf %lf", &a, &b) != EOF)
+    {
+        if((a || b) <= 0)
+        {
             fprintf(stderr,"Invalid rectangle sides!");
-        } else {
-    
-    rectangle(a,b,&p,&area);
-
-    printf("\nThe area is: %lf\n", area);
-    printf("The perimeter is: %lf\n", p);
-
+        }
+        else
+        {
+            rectangle(a, b, &p, &area);
+            printf("\nThe area is: %lf\n", area);
+            printf("The perimeter is: %lf\n", p);
         }
     }
-    
     return EXIT_SUCCESS;
 }
 
-int rectangle(double a,double b,double* p,double* area){
-    
-        *p = 2*(a + b);
-        *area = a*b;
+int rectangle(double a, double b, double* p, double* area)
+{   
+    *p = 2 *(a + b);
+    *area = a * b;
 
-        return 0;
-    }
+    return EXIT_SUCCESS;
+}
     
