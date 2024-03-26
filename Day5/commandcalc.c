6a9b0f4e8f56a7f359a781d4ad7cf90c62ed9ba9
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-26 13:58:55 +0200

    Update commandcalc.c

--- Day5/commandcalc.c
+++ Day5/commandcalc.c
@@ -6,36 +6,33 @@ Purpose: Command calculator
 #include <stdio.h>
 int main()
 {
-             double a;
-             double b;
-             double c;
-             char operation;
-            
-             
-            while(scanf("%lf %c %lf",&a, &operation, &b)!= EOF){
-
-
-            switch (operation)
-            {
+    double a;
+    double b;
+    double c;
+    char operation;
+                     
+    while(scanf("%lf %c %lf",&a, &operation, &b)!= EOF){
+    
+        switch (operation)
+        {
             case '+':
-              c=a+b;
-              printf("%lf\n",c);
-              break;
+                c = a + b;
+                printf("%lf\n", c);
+            break;
             case '-':
-              c=a-b;
-              printf("%lf\n",c);
-              break;
+                c = a - b;
+                printf("%lf\n", c);
+            break;
             case '*':
-              c=a*b;
-              printf("%lf\n",c);
-              break;
+                c = a * b;
+                printf("%lf\n", c);
+            break;
             case '/':
-              c=a/b;
-              printf("%lf\n",c);
-              break;
-          
-            default: fprintf(stderr,"Error! Invalid operation!");
-            }
-            }
+                c = a / b;
+                printf("%lf\n", c);
+            break;
+            default: fprintf(stderr, "Error! Invalid operation!");
+        }
+    }
     return EXIT_SUCCESS;
 }
