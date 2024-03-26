42f6b8b666a19f7862b9efea4004bfcd4021b2f6
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-26 13:42:21 +0200

    Update onescount.c

--- Day5/onescount.c
+++ Day5/onescount.c
@@ -7,51 +7,44 @@ Purpose: This program count how many 1 there are in the mask using variadic func
 #include <stdarg.h>
 #include <stdint.h>
 
-unsigned onesCount(size_t,unsigned, ...);
+unsigned onesCount(size_t, unsigned, ...);
 
 int main()
 {
-   onesCount(4,3,0x10011101111,0x111010,0x11101110);
-
-    return 0;
+   onesCount(4, 3, 0x10011101111, 0x111010, 0x11101110);
+   return 0;
 }
 
-unsigned onesCount(size_t size,unsigned count, ...){
-
-va_list args;
-    va_start(args, count);
-    for (int i = 0; i < count; i++){
-        if((size<1)||(size>sizeof(int))){
-        
-            fprintf(stderr,"Incorrect size of mask!");
+unsigned onesCount(size_t size,unsigned count, ...)
+{
+   va_list args;
+   va_start(args, count);
+      for (int i = 0; i < count; i++)
+      {
+         if((size < 1) || (size > sizeof(int)))
+         {
+            fprintf(stderr, "Incorrect size of mask!");
             return EXIT_FAILURE;
+         }
+         else
+         {
+            int bit = 1;
+            int bitcount = 0;
+            int lastbit = 0;
+            int args1 = va_arg(args, int);
 
-        } else {
-
-             int bit = 1;
-             int bitcount=0;
-             int lastbit=0;
-
-             int args1 = va_arg(args, int);
-
-             while(args1!=0){
-
-                 
-
-                 lastbit = args1 & bit;
-
-                 if (lastbit == 1){
+             while(args1 != 0)
+             {
+               lastbit = args1 & bit;
+                 if (lastbit == 1)
+                 {
                      bitcount++;
                  }
-
                args1 = args1 >> 1;
-
              }
-
-            printf("Number of 1 in the mask are: %d\n",bitcount);
-        }
-    }
-        va_end(args);
-    
-    return EXIT_SUCCESS;
+            printf("Number of 1 in the mask are: %d\n", bitcount);
+         }
+      }
+   va_end(args);  
+   return EXIT_SUCCESS;
 }
