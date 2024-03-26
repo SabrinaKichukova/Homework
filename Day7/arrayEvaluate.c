eb02c1c005ae3d52b69997de08e7e8f35924f849
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-26 14:48:45 +0200

    Update arrayEvaluate.c

--- Day7/arrayEvaluate.c
+++ Day7/arrayEvaluate.c
@@ -1,37 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void arrayEvaluate(int* arr, size_t n, int(*f)(int*,int));
+void arrayEvaluate(int* arr, size_t n, int(*f)(int*, int));
 int sum(int* arr, int n);
 
 int main()
 {   
     int arr[] = {50,25,7,3,66};
-
     size_t n = 5;
 
     arrayEvaluate(arr,n,sum);
-
     return 0;
-
 }
 
-int sum (int* arr, int n){
-
+int sum (int* arr, int n)
+{
     int sum = 0;
-
-    for(int i = 0; i <n ; i++){
-
+    for(int i = 0; i <n ; i++)
+    {
         sum += arr[i];
     }
-
     return sum;
-
 }
-void arrayEvaluate(int* arr, size_t n, int(*f)(int*,int)){
 
-printf("%d\n", f(arr,n)); 
-
+void arrayEvaluate(int* arr, size_t n, int(*f)(int*, int))
+{
+    printf("%d\n", f(arr,n)); 
 }
     
