f265a9628833fdf79afd98dc06212218448dfc67
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-26 17:24:42 +0200

    Update realtobit.c

--- Day10/realtobit.c
+++ Day10/realtobit.c
@@ -6,26 +6,25 @@ union realtobit
 {
     int32_t a;
     float b;
-
 } real1;
 
-int main(){
-
+int main()
+{
     real1.a = 0;
    
     printf("Please enter real number:\n");
-    scanf("%f",&real1.b);
+    scanf("%f", &real1.b);
 
     for (int i = 31; i >= 0; i--)
-  {
-    int k = real1.a >> i;
-
-    if (k & 1)
-      printf("1");
-    else
-      printf("0");
-  }
-  printf("\n");
-
+    {
+        int k = real1.a >> i;
+      
+        if (k & 1)
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
+    
     return 0;
 }
