73b6028a5f78d521f4eb80a8c4d88cdf0cbd5d2c
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-26 14:50:29 +0200

    Update secondMax.c

--- Day7/secondMax.c
+++ Day7/secondMax.c
@@ -1,34 +1,30 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 unsigned sumArrayDigits(const int* arr, size_t n);
 
 int main()
 {   
-    const int arr[] = {12, 34, 5, 70 };
-
-    printf("%d\n",sumArrayDigits(arr, 4));
+    const int arr[] = {12, 34, 5, 70};
     
+    printf("%d\n", sumArrayDigits(arr, 4));
     return 0;
 }
 
-unsigned sumArrayDigits(const int* arr, size_t n){
-
+unsigned sumArrayDigits(const int* arr, size_t n)
+{
     int sum = 0;
     int rem = 0;
 
-    for(int i = 0; i < n; i++){
-
+    for(int i = 0; i < n; i++)
+    {
         int num = *(arr + i);
-
-        while(num){
-
-        rem = num % 10;
-        sum += rem;
-        num = num /10;
-    
+        while(num)
+        {
+            rem = num % 10;
+            sum += rem;
+            num = num / 10;
         }
-    
     }
     return sum;
 }
