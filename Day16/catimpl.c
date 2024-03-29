a8fe6a8126945a5b8dd70475268ee3cfab982fb9
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 12:13:53 +0200

    Update catimpl.c

--- Day16/catimpl.c
+++ Day16/catimpl.c
@@ -5,30 +5,32 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-
+int main(int argc, char *argv[])
+{
     char buff[BUFSIZ];
     size_t bytes;
     int fd;
     
-
-    if(argc <= 1){
-            
-        while(bytes = read(STDIN_FILENO,buff,BUFSIZ) > 0 ){
-        write(STDOUT_FILENO,buff,bytes);
-          
+    if (argc <= 1)
+    {        
+        while (bytes = read(STDIN_FILENO, buff, BUFSIZ) > 0 )
+        {
+            write (STDOUT_FILENO, buff, bytes);
         }
-
-    } else {
-        for (int i = 0; i < argc; i++){
-            fd = open(argv[i],O_RDONLY);
-            if(fd == -1){
+    }
+    else
+    {
+        for (int i = 0; i < argc; i++)
+        {
+            fd = open(argv[i], O_RDONLY);
+            if (fd == -1) 
+            {
                 perror("Error: ");
                 return 1;
             }
-        
-            while ((bytes = read(fd,buff,BUFSIZ)) > 0){
-                write(STDOUT_FILENO,buff,bytes);
+            while ((bytes = read(fd, buff, BUFSIZ)) > 0)
+            {
+                write(STDOUT_FILENO, buff, bytes);
             }
         close(fd);
         }
