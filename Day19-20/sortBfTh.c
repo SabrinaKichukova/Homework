f184f0c1b609100d2a43caf039a314c31f2943d1
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 12:30:56 +0200

    Update sortBfTh.c

--- Day19-20/sortBfTh.c
+++ Day19-20/sortBfTh.c
@@ -9,32 +9,34 @@
 
 #define COUNT 10
 pthread_mutex_t mux;
-int cmpfunc (const void * a, const void * b);
+int cmpfunc (const void* a, const void* b);
 
 void* sort(void* arg)
 {   
     int* retval = malloc(sizeof(int));
     pthread_mutex_lock(&mux);
     FILE* srcfile = fopen((char*)arg, "rb");
-        if (NULL == srcfile){
-            perror("error");
-            *retval = -1;
-            return retval;
-        }
+    if (NULL == srcfile)
+    {
+        perror("error");
+        *retval = -1;
+        return retval;
+    }
     int num = 0;
     int buff[COUNT];
     int j = 0;
-    while (fread(&num, sizeof(int), 1, srcfile)){
-
+    while (fread(&num, sizeof(int), 1, srcfile))
+    {
         buff[j] = num;
         j++;              
     }
     fclose(srcfile);
 
-    qsort(buff,sizeof(buff),sizeof(int),cmpfunc);
+    qsort(buff, sizeof(buff), sizeof(int), cmpfunc);
 
     FILE* destfile = fopen((char*)arg, "wb");
-    if (NULL == srcfile){
+    if (NULL == srcfile)
+    {
         perror("error");
         *retval = -1;
         return retval;
@@ -44,29 +46,28 @@ void* sort(void* arg)
         fwrite(&buff[i], sizeof(int), 1, destfile);
     }
     pthread_mutex_unlock(&mux);
-    
 }
             
-
-
 int main(int argc, char const *argv[])
 {   
     pthread_mutex_init(&mux, NULL);
-    if(argc == 1){
+    if(argc == 1)
+    {
         perror("Missing arguments!");
         return EXIT_FAILURE;
     }   
     pthread_t th[argc - 1];
 
-    for (int i = 1; i < argc; i++) {
-        
+    for (int i = 1; i < argc; i++) 
+    {    
         pthread_create(th + i, NULL, &sort, &argv[i]);
-
     }
             
-    for (int i = 1; i < argc; i++){
+    for (int i = 1; i < argc; i++)
+    {
         void* retval;
-        if(pthread_join(th[i], &retval)){
+        if(pthread_join(th[i], &retval))
+        {
             perror("join error");
             return EXIT_FAILURE;
             free(&retval);
@@ -76,6 +77,7 @@ int main(int argc, char const *argv[])
     return EXIT_SUCCESS;
 } 
 
-int cmpfunc (const void * a, const void * b) {
+int cmpfunc (const void* a, const void* b) 
+{
    return ( *(int*)a - *(int*)b );
 }
