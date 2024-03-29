5a2854cd1c70b1ba47f17cdfb27d04f61a87b176
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 11:22:55 +0200

    Update checkbit.c

--- Day 12-13/checkbit.c
+++ Day 12-13/checkbit.c
@@ -3,30 +3,31 @@
 
  unsigned char CheckBit(u_int16_t);
 
-int main(void){
-
+int main(void)
+{
     u_int16_t uValue;
     printf("Enter number:\n");
     scanf("%hx", &uValue);
-    printf("%d\n",CheckBit(uValue));
+    printf("%d\n", CheckBit(uValue));
 
     return 0;
  }
 
- unsigned char CheckBit(u_int16_t uValue){
-
+ unsigned char CheckBit(u_int16_t uValue)
+{
     unsigned int count = 0;
-    while (uValue) {
+    while (uValue) 
+    {
         count += uValue & 1;
         uValue >>= 1;
     }
-    if (count == 1){
-
+    if (count == 1)
+    {
         return count;
-    } else {
+    }
+    else
+    {
         return 0;
     }
  }
-
-
  
