444c0a9908f0e47af82d11325538ed90e6d1f946
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 12:18:03 +0200

    Update lsimpl.c

--- Day16/lsimpl.c
+++ Day16/lsimpl.c
@@ -1,5 +1,3 @@
-
-  
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,53 +5,65 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-int main(void) {
+int main(void)
+{
     //ls only dir
     /*
-   DIR *dr;
-   struct dirent *en;
-   dr = opendir("/home/sabrina-kichukova");
-   if (dr == NULL) {
-       perror("error:");
-       return EXIT_FAILURE;
-   }
-   while ((en = readdir(dr)) != NULL) {
-       if (en->d_type == DT_DIR && en->d_name[0] != '.'){
-         printf("%s\n", en->d_name);
-       }
-   }
+    DIR *dr;
+    struct dirent *en;
+    dr = opendir("/home/sabrina-kichukova");
+    if (dr == NULL) 
+    {
+        perror("error:");
+        return EXIT_FAILURE;
+    }
+    while ((en = readdir(dr)) != NULL) 
+    {
+        if (en->d_type == DT_DIR && en->d_name[0] != '.')
+        {
+            printf("%s\n", en->d_name);
+        }
+    }
    closedir(dr);
 */
 // only fails without hidden files
 /*
-   DIR *dr;
-   struct dirent *en;
-   dr = opendir("/home/sabrina-kichukova");
-   if (dr == NULL) {
-       perror("error:");
-       return EXIT_FAILURE;
-   }
-   while ((en = readdir(dr)) != NULL) {
-       if (en->d_type == DT_REG && en->d_name[0] != '.'){
-         printf("%s\n", en->d_name);
-       }
+    DIR *dr;
+    struct dirent *en;
+    dr = opendir("/home/sabrina-kichukova");
+    
+    if (dr == NULL) 
+    {
+        perror("error:");
+        return EXIT_FAILURE;
+    }
+   while ((en = readdir(dr)) != NULL) 
+   {
+        if (en->d_type == DT_REG && en->d_name[0] != '.')
+        {
+            printf("%s\n", en->d_name);
+        }
    }
    closedir(dr);
 */
 //with hiden files
-DIR *dr;
-   struct dirent *en;
-   dr = opendir("/home/sabrina-kichukova");
-   if (dr == NULL) {
-       perror("error:");
-       return EXIT_FAILURE;
-   }
-   while ((en = readdir(dr)) != NULL) {
-       if (en->d_type == DT_REG && en->d_name[0]){
-         printf("%s\n", en->d_name);
-       }
-   }
-   closedir(dr);
+    DIR *dr;
+    struct dirent *en;
+    dr = opendir("/home/sabrina-kichukova");
+    
+    if (dr == NULL)
+    {
+        perror("error:");
+        return EXIT_FAILURE;
+    }
+    while ((en = readdir(dr)) != NULL)
+    {
+        if (en->d_type == DT_REG && en->d_name[0])
+        {
+            printf("%s\n", en->d_name);
+        }
+    }
+    closedir(dr);
 
-   return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
