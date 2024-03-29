c2f7b8b394e19313ba08e838f678440e19d75094
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 12:28:51 +0200

    Update nthreads.c

--- Day19-20/nthreads.c
+++ Day19-20/nthreads.c
@@ -10,32 +10,26 @@ void* func(void* arg)
 { 
     char random[COUNT];
 
-    for (int j = 0; j < COUNT; j++){
-        
+    for (int j = 0; j < COUNT; j++)
+    {    
         char randstring = 'a' + rand() % ('z' - 'a' + 1);
         random[j] = randstring;
         random[COUNT] = '\0';
     }
-
     printf("%s\n",random);
-
     sleep(1);
-     
 }
 
 int main(int argc, char const *argv[])
 {   
-  
     int n = atoi(*(argv + 1));
 
-    for (int i = 0; i < n; i++) {
-
+    for (int i = 0; i < n; i++) 
+    {
         srand(time(NULL));
         pthread_t th[i];
         pthread_create(&th[i], NULL, &func, NULL);
         pthread_join(th[i], NULL);
-
     }
-    
     return 0;
 }
