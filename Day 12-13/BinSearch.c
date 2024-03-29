61e69f9728ac66ca4b98592f001121a560a076dd
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 11:19:05 +0200

    Update BinSearch.c

--- Day 12-13/BinSearch.c
+++ Day 12-13/BinSearch.c
@@ -3,52 +3,47 @@
 
 unsigned int BinSearch(unsigned int *, unsigned int, unsigned int);   
 
-    int main()  
-    {  
-
-    unsigned int Array[] = {1,2,3,4,5,6,7,8,9,10};
+int main()  
+{  
+    unsigned int Array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     unsigned int uArraySize = sizeof(Array);
     unsigned int uValue = 8;
 
-    unsigned int result = BinSearch(Array,uArraySize,uValue);
+    unsigned int result = BinSearch(Array, uArraySize, uValue);
 
-    if (result == -1){
-    
-    fprintf (stderr,("There is no such value!"));
-    
-    } else {
-
+    if (result == -1)
+    {
+        fprintf (stderr, ("There is no such value!")); 
+    }
+    else
+    {
         printf("The index of element is: %d", result);
-
     }
     
     return 0;
-    }  
+}  
 
-    unsigned int BinSearch(unsigned int *pArray, unsigned int uArraySize, unsigned int uValue){
+unsigned int BinSearch(unsigned int *pArray, unsigned int uArraySize, unsigned int uValue)
+{
+    int right = uArraySize;
+    int left = *pArray;
 
-        int right = uArraySize;
-        int left = *pArray;
-
-        while(left<=right){
-
-            int middle = (left + (right - left) / 2);
-
-            if (uValue == middle){
-
-                return middle;
-
-            } else if(uValue > middle){
-
-                left = middle + 1;
-
-            } else {
-
-                right = middle - 1;
-            }
-            
-        }
-
-        return -1;
+    while(left <= right)
+    {
+        int middle = (left + (right - left) / 2);
         
+        if (uValue == middle)
+        {
+            return middle;
+        } 
+        else if(uValue > middle)
+        {
+            left = middle + 1;
+        }
+        else 
+        {
+            right = middle - 1;
+        }    
     }
+    return -1;   
+}
