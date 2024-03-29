722927f7d838ad84ca59ddaa5950788314beed90
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 11:49:33 +0200

    Update deque.c

--- Day14/deque.c
+++ Day14/deque.c
@@ -7,7 +7,7 @@ typedef struct node
     struct node* next;
     struct node* prev;
   
-}nodeDeque;
+} nodeDeque;
 
 void pushFirst(nodeDeque** head, int data);
 void pushBack(nodeDeque** head, int data);
@@ -15,50 +15,59 @@ void popFirst(nodeDeque** head);
 void popBack(nodeDeque** head);
 void printDeque(nodeDeque* head);
 
-int main(void){
+int main(void)
+{
+    nodeDeque* head = NULL;
+    pushFirst(&head, 5);
+    pushFirst(&head, 6);
+    pushFirst(&head, 7);
+    printDeque(head);
+    popFirst(&head);
+    printDeque(head);
+    pushBack(&head,8);
+    printDeque(head);
+    popBack(&head);
+    printDeque(head);
+    return 0;
+}
 
-nodeDeque* head = NULL;
-pushFirst(&head, 5);
-pushFirst(&head, 6);
-pushFirst(&head, 7);
-printDeque(head);
-popFirst(&head);
-printDeque(head);
-pushBack(&head,8);
-printDeque(head);
-popBack(&head);
-printDeque(head);
-return 0;
-
-}
-void pushFirst(nodeDeque** head, int data){
-nodeDeque* node = malloc(sizeof(nodeDeque));
-if(!node){
-     fprintf(stderr,"Error! Problem with memory allocation!");
-     exit(-1);
-}
-node->data = data;
-node->next = *head;
-node->prev = NULL;
-if(*head != NULL){
-    (*head)-> prev = node;
-}
-*head = node;
-}
-void pushBack(nodeDeque** head, int data){
-nodeDeque* node = malloc(sizeof(nodeDeque));
-nodeDeque* current = *head;
-nodeDeque* tail;
-if(!node){
-     fprintf(stderr,"Error! Problem with memory allocation!");
-     exit(-1);
-}
-if (current == NULL) {
-        *head = node;
+void pushFirst(nodeDeque** head, int data)
+{
+    nodeDeque* node = malloc(sizeof(nodeDeque));
+    
+    if (!node)
+    {
+         fprintf(stderr, "Error! Problem with memory allocation!");
+         exit(-1);
     }
-    else {
-      
-        while (current->next != NULL) {
+    node->data = data;
+    node->next = *head;
+    node->prev = NULL;
+    if (*head != NULL)
+    {
+        (*head)-> prev = node;
+    }
+    *head = node;
+}
+
+void pushBack(nodeDeque** head, int data)
+{
+    nodeDeque* node = malloc(sizeof(nodeDeque));
+    nodeDeque* current = *head;
+    nodeDeque* tail;
+    if (!node)
+    {
+         fprintf(stderr, "Error! Problem with memory allocation!");
+         exit(-1);
+    }
+    if (current == NULL) 
+    {
+            *head = node;
+    }
+    else
+    {  
+        while (current->next != NULL) 
+        {
             current = current->next;
         }
  
@@ -68,20 +77,23 @@ if (current == NULL) {
         node->next = NULL;
     }
 }
-
-void printDeque(nodeDeque* head){
-nodeDeque* current = head;
-
-while (current != NULL)
+    
+void printDeque(nodeDeque* head)
 {
-    printf("%d\n",current->data);
-    current = current->next;
+    nodeDeque* current = head;
+    
+    while (current != NULL)
+    {
+        printf("%d\n", current->data);
+        current = current->next;
+    }
+    putchar('\n');
 }
-putchar('\n');
 
-}
-void popFirst(nodeDeque** head){
-    if (*head == NULL){
+void popFirst(nodeDeque** head)
+{
+    if (*head == NULL)
+    {
         return;
     }
     *head = (*head)->next;
@@ -90,16 +102,19 @@ void popFirst(nodeDeque** head){
 
     (*head)->prev = NULL;   
 }
-void popBack(nodeDeque** head){
+
+void popBack(nodeDeque** head)
+{
     nodeDeque* current = (*head);
-    if (*head == NULL){
+    if (*head == NULL)
+    {
         return;
     }
-    while (current->next != NULL) {
-            current = current->next;
-        };
+    while (current->next != NULL)
+    {
+        current = current->next;
+    };
 
     current->prev->next = NULL;
     free(current);
-       
 }
