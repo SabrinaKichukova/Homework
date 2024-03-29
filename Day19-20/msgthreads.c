27e1d52c8b35eac2d64071db3d1aaa541a3c9ba2
Sabrina Kichukova <56067830+SabrinaKichukova@users.noreply.github.com>
2024-03-29 12:27:50 +0200

    Update msgthreads.c

--- Day19-20/msgthreads.c
+++ Day19-20/msgthreads.c
@@ -9,15 +9,14 @@
 #define COUNT 10
 
 void* func(void* arg)
-{ 
-    
-    printf("%s \n",(char*) arg);
-     
+{     
+    printf("%s \n",(char*) arg);   
 }
 
 int main(int argc, char const *argv[])
 {   
-    if(argc == 0){
+    if(argc == 0)
+    {
         perror("Missing arguments!");
         return EXIT_FAILURE;
     }
@@ -26,23 +25,23 @@ int main(int argc, char const *argv[])
     int P = atoi(*(argv + 1));
     int T = atoi(*(argv + 2));
 
-    for (int i = 0; i < P; i++){
+    for (int i = 0; i < P; i++)
+    {
         pid_t pid = fork();
-        if (0 == pid){
-
+        if (0 == pid)
+        {
             pthread_t th[T];
-            for (int i = 0; i < T; i++) {
-        
+            for (int i = 0; i < T; i++) 
+            {
                 pthread_create(&th[i], NULL, &func, arr);
                 pthread_join(th[i], NULL);
                 pthread_attr_destroy(th[i]);
             }
-         
              exit(EXIT_SUCCESS);
         }
-          
     }
-    for (int i = 0; i < P; i++){
+    for (int i = 0; i < P; i++)
+    {
         wait(NULL);
     }
     
